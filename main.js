c5a97bf340dba0e292fcbe7f34a3953d4977d0fb
rejinho1221 <reginho5167@gmail.com>
2025-06-07 14:54:10 -0300

    second commit

---
+++ main.js
@@ -0,0 +1,40 @@
+async function buscarClima(cidade) {
+    const apiKey = '04d033d1e38ca079d02eace057120716'; // Substitua pela sua API Key
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cidade)}&appid=${apiKey}&lang=pt_br&units=metric`;
+
+    try {
+        const resposta = await fetch(url);
+        if (!resposta.ok) {
+            throw new Error("Cidade não encontrada");
+        }
+
+        const dados = await resposta.json();
+        return dados;
+    } catch (erro) {
+        document.getElementById('msg-erro').innerHTML = `<p style="color:red;">Erro: ${erro.message}</p>`;
+        return null;
+    }
+}
+
+async function exibirResultados() {
+    const cidade = document.getElementById('cidade').value;
+
+    let form = document.querySelector('form');
+    document.getElementById('msg-erro').textContent = ''
+    document.getElementById('temperatura').textContent = '...';
+    document.getElementById('clima').textContent = '...';
+    document.getElementById('umidade').textContent = '...';
+    document.getElementById('vento').textContent = '...';
+
+    const dados = await buscarClima(cidade);
+
+    if (!dados){
+        form.reset();
+        return;
+    }
+
+    document.getElementById('temperatura').innerHTML = `<strong>${dados.main.temp} °C</strong>`;
+    document.getElementById('clima').innerHTML = `<strong>${dados.weather[0].description}</strong>`;
+    document.getElementById('umidade').innerHTML = `<strong>${dados.main.humidity}%</strong>`;
+    document.getElementById('vento').innerHTML = `<strong>${dados.wind.speed} km/h</strong>`;
+}
